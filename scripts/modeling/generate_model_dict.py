63e409f96e8489b60835e1d9807d9f1fe73ba94f
Ben Weeder <weederb23@gmail.com>
2020-08-24 15:08:15 -0700

    adds I/O for model weights and consolidated output

--- scripts/modeling/generate_model_dict.py
+++ scripts/modeling/generate_model_dict.py
@@ -1,16 +1,35 @@
+#!/usr/bin/env python3
+"""
+prep_epitope_validation_data.py
+
+For issues contact Ben Weeder (weeder@ohsu.edu)
+
+This script extracts MHC class I epitope examples from [DOI] and maps them to
+their source sequences. results are exported to a CSV and used downstream for
+model validation_prep
+"""
+
+from optparse import OptionParser
 import torch
 import os
 import pickle
 
-in_dir = "/Users/weeder/PycharmProjects/proteasome/models/model_weights"
-out_dir = "/Users/weeder/PycharmProjects/proteasome/models/deployed_models"
-model_dict = {}
-for f in os.listdir(in_dir):
-    if not f.startswith("."):
-        path = in_dir + "/" + f
-        mod_name, ext = os.path.splitext(f)
-        mod = torch.load(path)
-        model_dict[mod_name] = mod
+parser = OptionParser()
+parser.add_option("-i", "--in-dir",
+                  help="directory of pytorch model weights")
+parser.add_option("-o", "--out",
+                  help="output directory where summary dict will be exported")
 
-out_file = out_dir + "/trained_model_dict.pickle"
+(options, args) = parser.parse_args()
+
+model_dict = {}
+for f in os.listdir(options.in_dir):
+    if not f.startswith("."):
+        if f.endswith(".pt"):
+            path = options.in_dir + "/" + f
+            mod_name, ext = os.path.splitext(f)
+            mod = torch.load(path)
+            model_dict[mod_name] = mod
+
+out_file = options.out + "/trained_model_dict.pickle"
 pickle.dump(model_dict, open(out_file, 'wb'))
